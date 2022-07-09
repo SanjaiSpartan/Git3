4037f7c19a48ff6c3f3f272adfb8f4ad55a15e77
SanjaiSpartan <snjjvn@gmail.com>
2022-07-09 16:46:37 +0530

    mess

--- src/org/type/Types.java
+++ src/org/type/Types.java
@@ -4,6 +4,9 @@ public class Types extends TypesOfVariables {
 	public static void main(String[] args) {
 		System.out.println(TypesOfVariables.c);
 		System.out.println(c);
+		
+		int x = 10;
+		System.out.println(x);
 	}
 
 }
